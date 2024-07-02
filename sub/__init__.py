d7dc65c27818899fd38d313e79907ad43cbdeca5
Kiana Dane <69994311+kianadane@users.noreply.github.com>
2024-07-02 12:10:19 -0400

    push m09

---
+++ sub/__init__.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+print('WELCOME TO MY LIFE')
+
+print('there is only one function in this package.')
---
+++ sub/module_zzz.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def main():
+    ''' hello world function'''
+    print('Hello, world!')
+    print(main)
